a3312ada3edf80f2dfda16cfdc8cc892427c421e
Mehedimid <mehedihasan25276@gmail.com>
2025-06-18 22:27:09 +0600

    edited banner

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import {
   MdLocationOn,
   MdTravelExplore,
@@ -6,34 +7,62 @@ import {
 } from "react-icons/md";
 import { FaPaperPlane } from "react-icons/fa";
 import { BiWorld } from "react-icons/bi";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Banner = () => {
+  const images = [
+    "https://img.freepik.com/free-photo/backpacker-standing-sunrise-viewpoint-ja-bo-village-mae-hong-son-province-thailand_335224-1356.jpg?ga=GA1.1.1518679940.1727447932&semt=ais_hybrid&w=740",
+    "https://img.freepik.com/free-photo/skogafoss-waterfall-iceland-guy-red-jacket-looks-skogafoss-waterfall_335224-593.jpg?ga=GA1.1.1518679940.1727447932&semt=ais_hybrid&w=740",
+    "https://media.istockphoto.com/id/516449022/photo/lady-with-kayak.jpg?b=1&s=612x612&w=0&k=20&c=PsQQpGXVtV9dPDaH-DNEgeMAHjqkViCSQsSrkqlHKRk="
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => 
+      (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => 
+      (prevIndex + 1) % images.length
+    );
+  };
+
+    // Auto-slide every 3 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleNext();
+    }, 3000);
+
+    return () => clearInterval(interval); // Cleanup
+  }, [currentIndex]);
+
   return (
     <div
-      className="relative w-full h-[calc(100vh-80px)] md:h-[calc(100vh-100px)] lg:h-[calc(100vh-120px)] bg-cover bg-center "
+      className="relative w-full h-[calc(100vh-80px)] md:h-[calc(100vh-100px)] lg:h-[calc(100vh-120px)] bg-cover bg-center transition-all duration-500"
       style={{
-        backgroundImage:
-          "url('https://img.freepik.com/free-photo/backpacker-standing-sunrise-viewpoint-ja-bo-village-mae-hong-son-province-thailand_335224-1356.jpg?ga=GA1.1.1518679940.1727447932&semt=ais_hybrid&w=740')",
+        backgroundImage: `url('${images[currentIndex]}')`,
       }}
     >
       {/* Dark Overlay */}
       <div className="absolute inset-0 bg-black opacity-40"></div>
 
-      {/* Main Content (Text and Arrows) */}
+      {/* Main Content */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white px-4 text-center">
-        {/* Text Content */}
         <p className="text-red-400 font-bold mb-4 uppercase tracking-widest text-sm md:text-base">
           Tour & Travel Agency
         </p>
         <h1 className="text-4xl md:text-6xl lg:text-7xl font-extrabold leading-tight mb-4 tracking-wide">
-          Explore The <br className="md:hidden" /> Global Worlds
+          Explore Beauty<br className="md:hidden" />  of Bangladesh
         </h1>
         <p className="text-base md:text-lg max-w-2xl mx-auto text-gray-200">
           Velit integer eu tincidunt scelerisque. Sodales volutpat neque
           fermentum malesuada.
         </p>
 
-        {/* Small decorative elements (paper planes and dashed lines) */}
+        {/* Paper Planes */}
         <div className="absolute bottom-32 left-8 md:bottom-20 md:left-20 lg:left-40 xl:left-60">
           <FaPaperPlane className="text-white text-xl animate-float-one" />
           <div className="border-t border-dashed border-white w-24 mt-2 transform -rotate-12 animate-dash-left"></div>
@@ -44,21 +73,32 @@ const Banner = () => {
         </div>
       </div>
 
-      {/* Bottom Search/Filter Bar */}
-      <div className="mx-16 absolute bottom-0 left-0 right-0 z-20 bg-white/90 backdrop-blur-sm shadow-lg rounded-t-lg ">
+      {/* Prev/Next Buttons */}
+      <button
+        onClick={handlePrev}
+        className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white/50 hover:bg-white/80 text-black p-2 rounded-full z-20"
+      >
+        <FiChevronLeft size={24} />
+      </button>
+      <button
+        onClick={handleNext}
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white/50 hover:bg-white/80 text-black p-2 rounded-full z-20"
+      >
+        <FiChevronRight size={24} />
+      </button>
+
+      {/* Search Bar */}
+      <div className="mx-16 absolute bottom-0 left-0 right-0 z-20 bg-white/90 backdrop-blur-sm shadow-lg rounded-t-lg">
         <div className="flex flex-col md:flex-row items-center justify-between p-4 space-y-4 md:space-y-0 md:space-x-4">
-          {/* Destination Dropdown */}
           <div className="flex items-center text-gray-700 w-full md:w-auto border-b md:border-b-0 md:border-r border-gray-300 pb-2 md:pb-0 md:pr-4">
             <MdLocationOn className="mr-2 text-blue-500" size={20} />
             <select className="bg-transparent outline-none w-full cursor-pointer">
-              <option className="text-primary">Destination</option>
-              <option className="text-primary">Asia</option>
-              <option className="text-primary">Europe</option>
-              <option className="text-primary">America</option>
+              <option>Destination</option>
+              <option>Asia</option>
+              <option>Europe</option>
+              <option>America</option>
             </select>
           </div>
-
-          {/* Trip Types Dropdown */}
           <div className="flex items-center text-gray-700 w-full md:w-auto border-b md:border-b-0 md:border-r border-gray-300 pb-2 md:pb-0 md:pr-4">
             <MdTravelExplore className="mr-2 text-green-500" size={20} />
             <select className="bg-transparent outline-none w-full cursor-pointer">
@@ -68,8 +108,6 @@ const Banner = () => {
               <option>Cultural</option>
             </select>
           </div>
-
-          {/* Activity Dropdown */}
           <div className="flex items-center text-gray-700 w-full md:w-auto border-b md:border-b-0 md:border-r border-gray-300 pb-2 md:pb-0 md:pr-4">
             <BiWorld className="mr-2 text-purple-500" size={20} />
             <select className="bg-transparent outline-none w-full cursor-pointer">
@@ -79,8 +117,6 @@ const Banner = () => {
               <option>City Tour</option>
             </select>
           </div>
-
-          {/* Duration Dropdown */}
           <div className="flex items-center text-gray-700 w-full md:w-auto border-b md:border-b-0 border-gray-300 pb-2 md:pb-0 md:pr-4">
             <MdOutlineDateRange className="mr-2 text-orange-500" size={20} />
             <select className="bg-transparent outline-none w-full cursor-pointer">
@@ -89,8 +125,6 @@ const Banner = () => {
               <option>10+ Days</option>
             </select>
           </div>
-
-          {/* Find Your Trip Button */}
           <button className="bg-indigo-600 text-white font-semibold py-3 px-8 rounded-md hover:bg-indigo-700 transition-colors duration-300 w-full md:w-auto">
             Find Your Trip
           </button>
